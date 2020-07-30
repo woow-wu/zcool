2810839663f939d60465f1bb3522f0b5576cfd76
woow_wu7 <3107075636@qq.com>
2020-07-30 16:12:37 +0800

    deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+# 拉去代码
+git pull
+
+# 安装依赖
+npm --registry=https://registry.npm.taobao.org install
+
+# 打包
+npm --registry=https://registry.npm.taobao.org run build
+
+# 删除容器
+docker rm -f zcool
+
+# 映射容器 - 启动服务
+docker run -d --name=zcool \
+-v $PWD/dist:/usr/share/nginx/html \
+8cf1bfb43ff5
